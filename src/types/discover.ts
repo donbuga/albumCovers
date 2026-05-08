c680b01e48ebb0c3996596e671ca6f0c5bc0a544
donbuga <bugadj@gmail.com>
2026-05-08 12:13:13 -0400

    Improve discover map visualization

--- src/types/discover.ts
+++ src/types/discover.ts
@@ -1,10 +1,9 @@
 export interface CountryOption {
   code: string;
   name: string;
-  x: number;
-  y: number;
-  width: number;
-  height: number;
+  path: string;
+  labelX: number;
+  labelY: number;
 }
 
 export type DecadeOption = '1950s' | '1960s' | '1970s' | '1980s' | '1990s' | '2000s' | '2010s' | '2020s';
